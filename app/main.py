1aaa88449ef470eea5653646e1b7fe4185ad8958
Marcin <92676129+maciejewskimarcin@users.noreply.github.com>
2023-03-23 15:04:16 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,28 @@
+"""
+This module contains the main Flask application.
+"""
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    """
+    Display a simple greeting message.
+    """
     return 'Hi there! How are you today?'
 
 @app.route('/greet', methods=['GET', 'POST'])
 def greet():
+    """
+    Display a greeting message based on user input.
+    """
     if request.method == 'POST':
         name = request.form['name']
         greeting = f'Hello {name}! Have a great day!'
         return render_template('greet.html', greeting=greeting)
-    else:
-        return render_template('greet.html')
+    return render_template('greet.html')
 
 if __name__ == '__main__':
     app.run()
